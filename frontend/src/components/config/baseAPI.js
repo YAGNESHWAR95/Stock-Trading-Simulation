da9a3e62e470dd2344764515b32d47091860c010
YAGNESHWAR95 <yagneshwarreddy19@gmail.com>
2026-05-16 18:38:11 +0530

    first commit

--- frontend/src/components/config/baseAPI.js
+++ frontend/src/components/config/baseAPI.js
@@ -1,6 +1,4 @@
 // Determines the base URL depending on the environment
-const BASE_URL = import.meta.env.MODE === "development" 
-  ? "http://localhost:4000" 
-  : "https://stock-trading-app-1-isjb.onrender.com"; // Replace with your actual deployed URL
+const BASE_URL = "https://stock-trading-app-1-isjb.onrender.com" 
 
 export default BASE_URL;
