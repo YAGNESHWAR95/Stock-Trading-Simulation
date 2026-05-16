f5e04d7342f1b0b780714bc97e98463ee2429936
YAGNESHWAR95 <yagneshwarreddy19@gmail.com>
2026-05-16 18:34:00 +0530

    first commit

--- frontend/src/components/config/baseAPI.js
+++ frontend/src/components/config/baseAPI.js
@@ -1,6 +1,6 @@
 // Determines the base URL depending on the environment
 const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:4000" 
-  : "https://your-production-backend-url.vercel.app"; // Replace with your actual deployed URL
+  : "https://stock-trading-app-1-isjb.onrender.com"; // Replace with your actual deployed URL
 
 export default BASE_URL;
