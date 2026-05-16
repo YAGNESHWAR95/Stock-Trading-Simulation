983b64f7a860069ab9d6f8bc940f0be74c739009
YAGNESHWAR95 <yagneshwarreddy19@gmail.com>
2026-05-16 19:25:29 +0530

    backend deployment is completed

--- frontend/src/components/config/baseAPI.js
+++ frontend/src/components/config/baseAPI.js
@@ -1,6 +1,6 @@
 // Determines the base URL depending on the environment
 const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:4000" 
-  : ""; // Your live Render backend
+  : "https://stock-trading-simulation-ld2b.onrender.com"; // Your live Render backend
 
 export default BASE_URL;
